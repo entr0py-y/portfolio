0b833828484bdca7ed2f37a1cccb7695de914b35
Eren Yeager <erenyeager@Erens-MacBook-Air.local>
2026-05-02 15:34:58 +0530

    UI: remove old breadcrumb, bigger sylk.monster at full opacity aligned with nav icons

--- components/PageShell.tsx
+++ components/PageShell.tsx
@@ -7,18 +7,7 @@ interface PageShellProps {
 
 export default function PageShell({ title, children }: PageShellProps) {
   return (
-    <div className="w-full max-w-[650px] px-10 pt-10 pb-15 relative flex flex-col min-h-screen box-border z-[1] max-[768px]:px-5 max-[768px]:pt-4">
-      {/* Breadcrumb */}
-      <div className="mb-8 fade-in" style={{ fontFamily: "var(--font-body)" }}>
-        <span className="text-[12px] tracking-wide text-[var(--color-outline)]">
-          <Link href="/" className="text-[var(--color-outline)] no-underline hover:text-[var(--color-on-background)] transition-colors duration-200">
-            Pushkar
-          </Link>
-          <span className="mx-2 opacity-50">/</span>
-          <span className="text-[var(--color-on-background)]">{title}</span>
-        </span>
-      </div>
-
+    <div className="w-full max-w-[650px] px-10 pt-24 pb-10 relative flex flex-col min-h-screen box-border z-[1] max-[768px]:px-5 max-[768px]:pt-16 max-[768px]:pb-4">
       {/* Title */}
       <h1
         className="text-[24px] font-normal m-0 mb-6 leading-none fade-in fade-in-delay-1"
