9a6d8e8ae3c58a02124e95fe4e10abd3e2f87489
Eren Yeager <erenyeager@Erens-MacBook-Air.local>
2026-05-02 15:33:01 +0530

    UI: bold sylk.monster logo, revert signature to original positioning

--- app/page.tsx
+++ app/page.tsx
@@ -92,12 +92,7 @@ export default function Home() {
                 <img
                   src="/signature.png"
                   alt="Signature"
-                  className="signature-img absolute pointer-events-none opacity-85 rotate-[30deg] w-[300px] object-contain max-[768px]:w-[200px] max-[768px]:rotate-[25deg] max-[768px]:z-[20]"
-                  style={{
-                    left: "calc(200px + (100% - 200px) / 2)",
-                    top: "50%",
-                    transform: "translate(-50%, -50%) rotate(30deg)",
-                  }}
+                  className="signature-img absolute left-[200px] right-0 mx-auto w-[420px] object-contain opacity-85 rotate-[30deg] pointer-events-none max-[768px]:absolute max-[768px]:left-[140px] max-[768px]:right-0 max-[768px]:mx-auto max-[768px]:top-1/2 max-[768px]:-translate-y-1/2 max-[768px]:rotate-[25deg] max-[768px]:w-[240px] max-[768px]:z-[20]"
                 />
               </div>
               <div className="flex flex-col">
--- components/SiteLogo.tsx
+++ components/SiteLogo.tsx
@@ -15,7 +15,7 @@ export default function SiteLogo() {
       className="fixed top-10 left-10 z-[100] no-underline max-[768px]:top-5 max-[768px]:left-5"
       style={{ fontFamily: "var(--font-body)" }}
     >
-      <span className="text-[18px] font-normal leading-none">
+      <span className="text-[18px] font-semibold leading-none">
         <span className="text-[#aa8970]">sylk</span>
         <span className="text-[var(--color-on-background)] opacity-40">.monster</span>
         {segment && (
