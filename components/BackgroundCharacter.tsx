ef479eceeeac3c2e74589dd78a3148f5add29e25
Eren Yeager <erenyeager@Erens-MacBook-Air.local>
2026-05-03 11:19:00 +0530

    UI: restrict listen.png to only show on the /listen page, revert rest to character.png

--- components/BackgroundCharacter.tsx
+++ components/BackgroundCharacter.tsx
@@ -1,16 +1,27 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
 export default function BackgroundCharacter() {
+  const pathname = usePathname();
+  const isListenPage = pathname === "/listen";
+
   return (
     <div
-      className="fixed bottom-[-60px] right-[-50px] w-[575px] pointer-events-none z-0 scale-x-[-1] max-[768px]:w-[130vw] max-[768px]:max-w-[550px] max-[768px]:opacity-80 max-[768px]:bottom-[-30px] max-[768px]:right-[-30px]"
+      className={
+        isListenPage
+          ? "fixed bottom-[-60px] right-[-50px] w-[575px] pointer-events-none z-0 scale-x-[-1] max-[768px]:w-[130vw] max-[768px]:max-w-[550px] max-[768px]:opacity-80 max-[768px]:bottom-[-30px] max-[768px]:right-[-30px]"
+          : "fixed bottom-[-20px] right-[-20px] w-[575px] pointer-events-none z-0 scale-x-[-1] max-[768px]:w-[130vw] max-[768px]:max-w-[550px] max-[768px]:opacity-80 max-[768px]:bottom-[-10px]"
+      }
       aria-hidden="true"
     >
       {/* Using img tag to match original behavior exactly */}
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        src="/listen.png"
+        src={isListenPage ? "/listen.png" : "/character.png"}
         alt=""
-        className="w-full h-auto block bg-character-img"
-        style={{ imageRendering: "pixelated", opacity: 0.14 }}
+        className={isListenPage ? "w-full h-auto block bg-character-img" : "w-full h-auto block"}
+        style={{ imageRendering: "pixelated", opacity: isListenPage ? 0.14 : 0.12 }}
       />
     </div>
   );
