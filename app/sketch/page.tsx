d280dcc19626d19d23ce89cfa660d869f259d53b
Eren Yeager <erenyeager@Erens-MacBook-Air.local>
2026-08-31 22:48:23 +0530

    Further space out sketches to prevent overlap on mobile

--- app/sketch/page.tsx
+++ app/sketch/page.tsx
@@ -13,10 +13,10 @@ const sketches = [
   { src: "/sketch/sketch6.jpeg", top: "34%", left: "48%", rotate: "5deg", z: 30 },
   { src: "/sketch/sketch7.jpeg", top: "48%", left: "-5%", rotate: "-8deg", z: 20 },
   { src: "/sketch/sketch8.jpeg", top: "51%", left: "40%", rotate: "12deg", z: 35 },
-  { src: "/Sketch/23.jpg", top: "65%", left: "-6%", rotate: "-10deg", z: 10 },
-  { src: "/Sketch/24.jpg", top: "71%", left: "48%", rotate: "6deg", z: 25 },
-  { src: "/Sketch/25.jpg", top: "86%", left: "-8%", rotate: "15deg", z: 15 },
-  { src: "/Sketch/26.jpg", top: "89%", left: "42%", rotate: "-5deg", z: 30 },
+  { src: "/Sketch/23.jpg", top: "69%", left: "-12%", rotate: "-10deg", z: 10 },
+  { src: "/Sketch/24.jpg", top: "75%", left: "48%", rotate: "6deg", z: 25 },
+  { src: "/Sketch/25.jpg", top: "92%", left: "-8%", rotate: "15deg", z: 15 },
+  { src: "/Sketch/26.jpg", top: "96%", left: "40%", rotate: "-5deg", z: 30 },
 ];
 
 export default function SketchingPage() {
@@ -35,7 +35,7 @@ export default function SketchingPage() {
         </p>
 
         {/* Scattered Collage */}
-        <div className="relative w-full h-[1850px] max-[768px]:h-[1550px] mb-[300px] max-[768px]:mb-[250px]">
+        <div className="relative w-full h-[2000px] max-[768px]:h-[1700px] mb-[300px] max-[768px]:mb-[250px]">
           {sketches.map((sketch, i) => (
             <div
               key={i}
